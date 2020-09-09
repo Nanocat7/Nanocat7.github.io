70567905aa41a853ef2223fa73091ab960039a3a
Prateek Kesavarapu <prateekkesavarapu@gmail.com>
2020-09-09 18:17:00 +0530

    Looks nice

--- dropdown.js
+++ dropdown.js
@@ -1,19 +1,28 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+ var myVar;
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+ function myFunction() {
+   myVar = setTimeout(showPage, 3000);
+ }
+
+ function showPage() {
+   document.getElementById("loader").style.display = "none";
+   document.getElementById("myDiv").style.display = "block";
+ }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {Scroll()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function Scroll() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
 }
